d2bbc4ff333911925244f61962fe0a41daef2fd1
Kara Watts <kdwwatts@gmail.com>
2023-11-25 19:00:49 -0500

    added First Reverse

---
+++ First-Reverse/index.js
@@ -0,0 +1,8 @@
+function FirstReverse(str) { 
+    let arr = str.split('')
+    return arr.reverse().join('')
+    
+    }
+       
+    // keep this function call here 
+    console.log(FirstReverse(readline()));
